fe076517d958801c2756e0d5ecfcd45e3df250b0
ruslan <ruslanusen145@gmail.com>
2026-04-07 21:25:56 +0500

    add tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import Login from ".pages/Login";
+import Login from "./pages/login";
 
 function App() {
-  return <Login />;
+  return (
+    <div className="h-screen bg-green-500 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-xl text-center">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4">
+          Tailwind работает 🚀
+        </h1>
+
+        <p className="text-gray-600 mb-6">
+          Если ты видишь стили — всё настроено правильно
+        </p>
+
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition">
+          Кнопка
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
